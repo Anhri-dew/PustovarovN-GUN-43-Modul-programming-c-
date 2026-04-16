bbd70294d34fa44aac739ce7aec7fc80adb44b42
Anhri-dew <78682590+Anhri-dew@users.noreply.github.com>
2026-04-16 15:23:08 +0500

    convert.toString
    Калькулятор побитовых операций, работа с побитовыми операциями, конвертация в разные системы счисления

--- Modul-programing-C#/Program.cs
+++ Modul-programing-C#/Program.cs
@@ -2,84 +2,62 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Наша программа может использовать следующие математические операции (+ - * / %) введите 2 числа и знак математической операции ");
 
-        //var a = Int32.Parse(Console.ReadLine());
-        //var b = Int32.Parse(Console.ReadLine());
-        //var result = 0;
-        //Console.WriteLine("result {0} + {1} = {2}", a, b, result = a + b);
-        //Console.WriteLine("result {0} - {1} = {2}", a, b, a - b);
-        //Console.WriteLine("result {0} * {1} = {2}", a, b, a * b);
-        //Console.WriteLine("result {0} / {1} = {2}", a, b, a / b);
-        //Console.WriteLine("result {0} % {1} = {2}", a, b, a % b);
-        //Console.WriteLine(result);
+        
+        if (Int32.TryParse(Console.ReadLine(), out var a))
+        {
+            Console.WriteLine($"введенное число в 2-чной форме {Convert.ToString(a, 2)}");
+            Console.WriteLine($"введенное число в 10-чной форме {a}");
+            Console.WriteLine($"введенное число в 16-ричной форме {Convert.ToString(a, 16)}");
+        }
+        else
+        {
+            Console.WriteLine("введено не число");
+            return; // если тут убрать ретерн продолжится выполнение программы
+        }
 
-
-        if (!Int32.TryParse(Console.ReadLine(), out var a))
+        if (Int32.TryParse(Console.ReadLine(), out var b))
+        {
+            Console.WriteLine($"введенное число в 2-чной форме {Convert.ToString(b,2)}");
+            Console.WriteLine($"введенное число в 10-чной форме {b}");
+            Console.WriteLine($"введенное число в 16-ричной форме {Convert.ToString(b, 16)}");
+        }
+        else
         {
             Console.WriteLine("введено не число");
             return;
         }
-        if (!Int32.TryParse(Console.ReadLine(), out var b))
-        {
-            Console.WriteLine("введено не число");
-            return;
-        }
+
+        Console.WriteLine();
 
 
-        //if (Int32.TryParse(Console.ReadLine(), out var a))
-        //{
-        //    Console.WriteLine($"введенное число {a}");
-        //}
-        //else
-        //{
-        //    Console.WriteLine("введено не число");
-        //    return; // если тут убрать ретерн продолжится выполнение программы
-        //}
-
-        //if (Int32.TryParse(Console.ReadLine(), out var b))
-        //{
-        //    Console.WriteLine($"введенное число {b}");
-        //}
-        //else
-        //{
-        //    Console.WriteLine("введено не число");
-        //    return;
-        //}
-
-
+        Console.WriteLine("Введите побитовый оператор & | или ^");
         var s = Console.ReadLine();
-        //var boolVar = true;
-        if (s.Length == 0 || s.Length > 1 /*&& !boolVar непонял зачем нам нужен этот братиш*/)
+
+        if (s.Length == 0 || s.Length > 1 || (s[0] != '&' && s[0] != '|' && s[0] != '^')) 
         {
-            Console.WriteLine("Введен не коректный знак математической операции");
+            Console.WriteLine("Введен не коректный знак побитовой операции");
             return;
         }
 
         switch (s[0])
         {
-            case '+':
-                Console.WriteLine("result {0} + {1} = {2}", a, b, a + b);
+            case '&':
+                Console.WriteLine("результат вычисленгия в 2-чной форме {0} & {1} = {2}", Convert.ToString(a,2), Convert.ToString(b,2), Convert.ToString(a & b ,2));
+                Console.WriteLine("результат вычисленгия в 10-чной форме {0} & {1} = {2}", a, b, a & b);
+                Console.WriteLine("результат вычисленгия в 16-чной форме {0} & {1} = {2}", Convert.ToString(a, 16), Convert.ToString(b, 16), Convert.ToString(a & b, 16));
                 break;
 
-            case '-':
-                Console.WriteLine("result {0} - {1} = {2}", a, b, a - b);
+            case '|':
+                Console.WriteLine("результат вычисленгия в 2-чной форме {0} | {1} = {2}", Convert.ToString(a, 2), Convert.ToString(b, 2), Convert.ToString(a | b, 2));
+                Console.WriteLine("результат вычисленгия в 10-чной форме {0} | {1} = {2}", a, b, a | b);
+                Console.WriteLine("результат вычисленгия в 16-чной форме {0} | {1} = {2}", Convert.ToString(a, 16), Convert.ToString(b, 16), Convert.ToString(a | b, 16));
                 break;
 
-            case '*':
-                Console.WriteLine("result {0} * {1} = {2}", a, b, a * b);
-                break;
-
-            case '/':
-                Console.WriteLine("result {0} / {1} = {2}", a, b, a / b);
-                break;
-
-            case '%':
-                Console.WriteLine("result {0} % {1} = {2}", a, b, a % b);
-                break;
-
-            default:
-                Console.WriteLine("К сожалению введенный знак либо не является математическим, либо наша программа еще не умеет его обрабатывать, следите за обновлениями :)");
+            case '^':
+                Console.WriteLine("результат вычисленгия в 2-чной форме {0} ^ {1} = {2}", Convert.ToString(a, 2), Convert.ToString(b, 2), Convert.ToString(a ^ b, 2));
+                Console.WriteLine("результат вычисленгия в 10-чной форме {0} ^ {1} = {2}", a, b, a ^ b);
+                Console.WriteLine("результат вычисленгия в 16-чной форме {0} ^ {1} = {2}", Convert.ToString(a, 16), Convert.ToString(b, 16), Convert.ToString(a ^ b, 16));
                 break;
         }
     }
